a26b63c8f58a976bb1db8ca3e7bebbe4afeaab76
dharmendra007 <mahantas025@gmail.com>
2024-12-22 14:21:19 +0530

    feat: add hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      fontFamily :{
+      fontFamily: {
+        zentry: ["zentry", "sans-serif"],
+        general: ["general", "sans-serif"],
+        "circular-web": ["circular-web", "sans-serif"],
+        "robert-medium": ["robert-medium", "sans-serif"],
+        "robert-regular": ["robert-regular", "sans-serif"],
         'anime' : ['anime','sanf-serif']
       },
-      colors : {
-        yellow: {
-          100: '#8E983F',
-          300: '#EDFF66'
-        },
+      colors: {
         blue: {
-          50: '#DFDFF0',
-          75: '#DFDFF2',
-          100: '#F0F2FA',
-          200: '#4FB7DD'
+          50: "#DFDFF0",
+          75: "#dfdff2",
+          100: "#F0F2FA",
+          200: "#010101",
+          300: "#4FB7DD",
         },
-
-      }
+        violet: {
+          300: "#5724ff",
+        },
+        yellow: {
+          100: "#8e983f",
+          300: "#edff66",
+        },
+      },
     },
   },
   plugins: [],
-}
+};
