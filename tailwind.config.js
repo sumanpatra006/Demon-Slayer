e6378a8819aaf59d3009a8436c1806754330b3a5
dharmendra007 <mahantas025@gmail.com>
2024-12-28 21:34:54 +0530

    feat: add Hashira section

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,6 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      animation: {
-        "meteor-effect": "meteor 5s linear infinite",
-      },
-      
-      keyframes: {
-        meteor: {
-          "0%": { transform: "rotate(215deg) translateX(0)", opacity: "1" },
-          "70%": { opacity: "1" },
-          "100%": {
-            transform: "rotate(215deg) translateX(-500px)",
-            opacity: "0",
-          },
-        },
-      },
       fontFamily: {
         zentry: ["zentry", "sans-serif"],
         general: ["general", "sans-serif"],
