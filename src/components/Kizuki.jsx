36322a8842168dcf7ca4ab34352b3f6acc0f1a10
dharmendra007 <mahantas025@gmail.com>
2025-01-15 00:27:31 +0530

    fix: fix box and kizuki section

--- src/components/Kizuki.jsx
+++ src/components/Kizuki.jsx
@@ -4,7 +4,7 @@ import kizukis from '../../constants'
 
 function Kizuki() {
   return (
-    <div className='relative h-auto w-screen bg-slate-900 flex flex-col  gap-16 border border-spacing-y-1'>
+    <div className='relative h-auto w-screen bg-slate-900 flex flex-col py-4 gap-16 border border-spacing-y-1'>
       <span>
       <p className='text-white text-center font-anime text-[2.3rem]'>Blood and shadows entwine to form our art. <br />We are the <span className='text-red-500 bg-white p-2'>Kizukis!</span></p>
       </span>
